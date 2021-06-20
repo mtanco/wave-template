b83c4651cbcd38038e10df5c98b30705e4bedb62
Michelle Tanco <Michelle.Tanco@h2o.ai>
2021-06-20 08:44:03 -0700

    Add template for use-case specific configurations

---
+++ src/usecase_config.py
@@ -0,0 +1,17 @@
+class UsecaseConfiguration:
+    def __init__(self):
+
+        # App Details
+        self.app_title = ""
+        self.app_subtitle = ""
+        self.app_icon = "ContactHeart"
+        self.dark_mode = False
+        self.app_ga = ""
+
+        # Look and Feel
+        self.color_primary = "#FFE600"
+        self.color_secondary = "#000000"
+
+
+
+
